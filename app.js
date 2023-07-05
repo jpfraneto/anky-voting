226cd098518371de5fbac262d991ce9e763e1349
jpfraneto <jpfraneto@gmail.com>
2023-07-05 13:15:36 -0400

    world 7 is ready to rumble

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ import('dotenv').then(dotenv => dotenv.config());
 import express from 'express';
 import { backupCreator } from './lib/backuper.js';
 import { imageUploader } from './lib/uploadToCloudinary.js';
+import { processVotedCharacters } from './lib/processVotedCharacters.js';
 import { prisma } from './lib/prismaClient.js';
 import routes from './routes.js';
 
 const app = express();
-const port = 3000;
+const port = 3008;
 
 app.use(express.json());
 app.use(routes);
@@ -15,28 +16,41 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // backupCreator();
-// imageUploader(8);
-main();
+// imageUploader(7);
+// main();
+// processVotedCharacters();
 
-async function main() {
-  console.log('inside the main function');
-  const characters = await prisma.character.findMany({
-    where: { world: 8 },
-    include: { images: true },
+// async function main() {
+//   console.log('inside the main function');
+//   const characters = await prisma.character.findMany({
+//     where: { world: 7 },
+//   });
+//   console.log(`There are ${characters.length} characters in the 8 world.`);
+//   const votedCharacters = characters.filter(x => x.voted === 1);
+//   console.log(`Out of these, ${votedCharacters.length} have been voted.`);
+//   console.log(votedCharacters[0]);
+// }
+
+function checkForTie(character) {
+  let votesForOne = 0;
+
+  // We count how many images have exactly 1 vote
+  character.images.forEach(image => {
+    if (image.votes === 1) votesForOne++;
   });
-  console.log(`There are ${characters.length} characters in the 8 world.`);
-  const votedCharacters = characters.filter(x => x.voted === 1);
-  console.log(`Out of these, ${votedCharacters.length} have been voted.`);
-  console.log(votedCharacters[0]);
+
+  // If more than one image has exactly 1 vote, we have a tie
+  if (votesForOne > 1) return true;
+
+  return false; // There is no tie
 }
 
 app.get('/', async (req, res) => {
-  const characters = await prisma.character.findMany({
-    where: { world: 8, voted: 0 },
+  let characters = await prisma.character.findMany({
+    where: { world: 7, voted: 0 },
     include: { images: true },
     take: 10,
   });
-
   if (characters && characters.length) {
     res.render('index', { characters });
   } else {
--- routes.js
+++ routes.js
@@ -3,15 +3,33 @@ import { prisma } from './lib/prismaClient.js';
 
 const router = express.Router();
 
+function checkForTie(character) {
+  let votesForOne = 0;
+
+  // We count how many images have exactly 1 vote
+  character.images.forEach(image => {
+    if (image.votes === 1) votesForOne++;
+  });
+
+  // If more than one image has exactly 1 vote, we have a tie
+  if (votesForOne > 1) return true;
+
+  return false; // There is no tie
+}
+
 // Route to get images with 0 votes
 router.get('/api/characters', async (req, res) => {
   try {
-    const characters = await prisma.character.findMany({
-      where: { voted: 0, world: 8 },
+    let characters = await prisma.character.findMany({
+      where: { world: 7, voted: 0 },
       include: { images: true },
       take: 10,
     });
-    res.json(characters);
+    if (characters && characters.length) {
+      res.json(characters);
+    } else {
+      res.json([]);
+    }
   } catch (error) {
     res.status(500).send(error);
   }
